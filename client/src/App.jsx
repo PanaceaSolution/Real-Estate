f3ac21747e7aff8e125ffb2468734143a01f2f31
saurashya <saurashya1@gmail.com>
2024-09-08 23:37:11 +0545

    git folder setup,Saurashya

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-3xl flex items-center justify-center h-screen'>App</div>
+  )
+}
+
+export default App
