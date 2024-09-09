12afbad923b554f9fd7ea481e1bb617a23f4cc13
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-09-09 14:46:35 +0545

    Feat:Tested error handler in '/'route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-import express from 'express'
+import express from "express";
+import { handleError } from "./middlewares/errorHandler.js";
 
-const app=express();
+const app = express();
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get("/", handleError, (req, res) => {
+  res.send("This route is working fine");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
